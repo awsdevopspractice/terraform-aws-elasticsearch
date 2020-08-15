efd0fafaf457453b04c7f8fb9948b454b3f571ae
Franco Bontorin <bontorin@amazon.com>
2020-08-15 12:42:03 -0400

    fixing tls plcy

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ resource "aws_elasticsearch_domain" "es_domain" {
   # Enabling HTTPS
   domain_endpoint_options {
     enforce_https = true
+    tls_security_policy = "Policy-Min-TLS-1-2-2019-07"
   }
 
   # Domain name
