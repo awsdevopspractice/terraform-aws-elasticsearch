347ca044e70b1f06699f2cbaa10a4adc3cac19d5
Franco Bontorin <bontorin@amazon.com>
2020-08-16 12:27:17 -0400

    new provider

--- examples/advanced_security_options/master_user_name_pasword/variables.tf
+++ examples/advanced_security_options/master_user_name_pasword/variables.tf
@@ -1,12 +1,8 @@
 # Provider
 variable "region" {}
-variable "profile" {}
-
 
 # AWS Elasticsearch
 variable "es_domain_name" {}
-variable "es_version" {}
-
 
 # Whitelist (allow public IPs)
 variable "whitelist" {
